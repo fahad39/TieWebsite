f080d83c660d78f86e1acd8df9abc277b2a7bbe7
fahad39 <fahadhussain0127@gmail.com>
2025-03-20 21:42:51 +0500

    workinng on product box

--- client/components/ProductBox.js
+++ client/components/ProductBox.js
@@ -2,11 +2,38 @@ import styled from "styled-components";
 import Button from "@/components/Button";
 import CartIcon from "@/components/icons/CartIcon";
 import Link from "next/link";
-import {useContext} from "react";
-import {CartContext} from "@/components/CartContext";
+import { useContext, useState } from "react";
+import { CartContext } from "@/components/CartContext";
 
 const ProductWrapper = styled.div`
-  
+  position: relative;
+`;
+
+const HeartIcon = styled.div`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background-color: white;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+  z-index: 5;
+
+  &:hover {
+    color: red;
+  }
+
+  ${(props) =>
+    props.isFavorite
+      ? `
+    color: red;
+  `
+      : ""}
 `;
 
 const WhiteBox = styled(Link)`
@@ -18,7 +45,7 @@ const WhiteBox = styled(Link)`
   align-items: center;
   justify-content: center;
   border-radius: 10px;
-  img{
+  img {
     max-width: 100%;
     max-height: 80px;
   }
@@ -26,10 +53,10 @@ const WhiteBox = styled(Link)`
 
 const Title = styled(Link)`
   font-weight: normal;
-  font-size:.9rem;
-  color:inherit;
-  text-decoration:none;
-  margin:0;
+  font-size: 0.9rem;
+  color: inherit;
+  text-decoration: none;
+  margin: 0;
 `;
 
 const ProductInfoBox = styled.div`
@@ -43,39 +70,83 @@ const PriceRow = styled.div`
     gap: 5px;
   }
   align-items: center;
-  justify-content:space-between;
-  margin-top:2px;
+  justify-content: space-between;
+  margin-top: 2px;
 `;
 
 const Price = styled.div`
   font-size: 1rem;
-  font-weight:400;
+  font-weight: 400;
   text-align: right;
   @media screen and (min-width: 768px) {
     font-size: 1.2rem;
-    font-weight:600;
+    font-weight: 600;
     text-align: left;
   }
 `;
 
-export default function ProductBox({_id,title,description,price,images}) {
-  const {addProduct} = useContext(CartContext);
-  const url = '/product/'+_id;
+export default function ProductBox({ _id, title, description, price, images }) {
+  const { addProduct } = useContext(CartContext);
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [isAddedToCart, setIsAddedToCart] = useState(false);
+  const url = "/product/" + _id;
+
+  const handleAddToCart = () => {
+    addProduct(_id);
+    setIsAddedToCart(true);
+  };
+
+  const toggleFavorite = (ev) => {
+    ev.preventDefault();
+    ev.stopPropagation();
+    setIsFavorite((prev) => !prev);
+  };
+
   return (
     <ProductWrapper>
+      <HeartIcon onClick={toggleFavorite} isFavorite={isFavorite}>
+        {isFavorite ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fillRule="evenodd"
+              d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            viewBox="0 0 16 16"
+          >
+            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+          </svg>
+        )}
+      </HeartIcon>
       <WhiteBox href={url}>
         <div>
-          <img src={images?.[0]} alt=""/>
+          <img src={images?.[0]} alt="" />
         </div>
       </WhiteBox>
       <ProductInfoBox>
         <Title href={url}>{title}</Title>
         <PriceRow>
-          <Price>
-            ${price}
-          </Price>
-          <Button block onClick={() => addProduct(_id)} primary outline>
-            Add to cart
+          <Price>${price}</Price>
+          <Button
+            block
+            onClick={handleAddToCart}
+            primary
+            outline
+            disabled={isAddedToCart}
+          >
+            {isAddedToCart ? "Added to cart" : "Add to cart"}
           </Button>
         </PriceRow>
       </ProductInfoBox>
