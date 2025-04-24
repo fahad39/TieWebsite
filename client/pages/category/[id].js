c1de10fec1d639f5b694602c930f8b24e867c981
fahad39 <fahadhussain0127@gmail.com>
2025-04-24 11:49:17 +0500

    category description page completed

--- client/pages/category/[id].js
+++ client/pages/category/[id].js
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import Center from "@/components/Center";
 import Header from "@/components/Header";
 import Title from "@/components/Title";
 import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
+import { Category } from "@/models/Category";
 import styled from "styled-components";
 import WhiteBox from "@/components/WhiteBox";
 import ProductImages from "@/components/ProductImages";
@@ -10,42 +12,77 @@ import Button from "@/components/Button";
 import CartIcon from "@/components/icons/CartIcon";
 import { useContext } from "react";
 import { CartContext } from "@/components/CartContext";
+import ProductsGrid from "@/components/ProductsGrid";
 
-const ColWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 1fr;
-  @media screen and (min-width: 768px) {
-    grid-template-columns: 0.8fr 1.2fr;
+const CategoryDescriptonStyled = styled.div`
+  .filter-div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
   }
-  gap: 40px;
-  margin: 40px 0;
-`;
-const PriceRow = styled.div`
-  display: flex;
-  gap: 20px;
-  align-items: center;
-`;
-const Price = styled.span`
-  font-size: 1.4rem;
 `;
 
-export default function CategoryPage({ product }) {
+export default function CategoryPage({ category, products }) {
   const { addProduct } = useContext(CartContext);
+  const [filteredProducts, setFilteredProducts] = useState(products);
+  console.log(products);
+
+  const handleFilter = (filterType) => {
+    let sortedProducts = [...products];
+    if (filterType === "high-to-low") {
+      sortedProducts.sort((a, b) => b.price - a.price);
+    } else if (filterType === "low-to-high") {
+      sortedProducts.sort((a, b) => a.price - b.price);
+    } else if (filterType === "latest") {
+      sortedProducts.sort(
+        (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+      );
+    } else if (filterType === "oldest") {
+      sortedProducts.sort(
+        (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
+      );
+    }
+    setFilteredProducts(sortedProducts);
+  };
   return (
-    <>
+    <CategoryDescriptonStyled>
       <Header />
-      <Center>Catefory Description page</Center>
-    </>
+      <Center>
+        <div className="filter-div">
+          <h1>{category.name}</h1>
+          <div>
+            <select
+              onChange={(e) => handleFilter(e.target.value)}
+              style={{ padding: "5px", fontSize: "1rem" }}
+            >
+              <option value="">Filter By</option>
+              <option value="high-to-low">Price: High to Low</option>
+              <option value="low-to-high">Price: Low to High</option>
+              <option value="latest">Latest</option>
+              <option value="oldest">Oldest</option>
+            </select>
+          </div>
+        </div>
+        <ProductsGrid products={filteredProducts} />
+      </Center>
+    </CategoryDescriptonStyled>
   );
 }
 
 export async function getServerSideProps(context) {
   await mongooseConnect();
   const { id } = context.query;
-  const product = await Product.findById(id);
+  const category = await Category.findById(id).lean();
+  if (!category) {
+    return {
+      notFound: true, // Return a 404 page if the category is not found
+    };
+  }
+  const products = await Product.find({ category: category._id }).lean();
   return {
     props: {
-      product: JSON.parse(JSON.stringify(product)),
+      category: JSON.parse(JSON.stringify(category)),
+      products: JSON.parse(JSON.stringify(products)),
     },
   };
 }
